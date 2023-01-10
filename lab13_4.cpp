af2140ac3964b5cef4bdca90eb78a5b62800d591
Okinawa <fitthep@gmail.com>
2023-01-10 16:11:05 +0700

    lab13_4 completed

--- lab13_4.cpp
+++ lab13_4.cpp
@@ -2,6 +2,9 @@
 
 using namespace std;
 
+template<typename currentType>
+void mySwap(currentType &,currentType &); 
+
 int main(){
 	int x, y;
 	string a, b;
@@ -29,4 +32,9 @@ int main(){
 
 	return 0;
 }
-
+template<typename currentType>
+void mySwap(currentType &x,currentType &y){
+	currentType first=x;
+	x=y;
+	y=first;
+}
